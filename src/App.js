63972592fedaffe141f6c5055219e7dec352aac6
Muhammad Amien Rauf <muhammadamien23@gmail.com>
2021-10-14 16:38:39 +0700

    upload baru

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todo from './components/todo/Todo.js';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/todo/Todo.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import TodoCreate from "../todo-create/TodoCreate";
+import TodoList from "../todo-list/TodoList";
+import "./Todo.css";
+
+const Todo = () => {
+    const [getTodos, setTodos] = useState([
+        { id: 1, title: 'Eat' },
+        { id: 2, title: 'Sleep' },
+        { id: 3, title: 'Code' },
+    ])
+
+    const eventCreateTodo = (todo) => {
+        setTodos(getTodos.concat(todo))
+        // console.log(todo)
+    }
+
+    return (
+        <div>
+            <h3>Todo List</h3>
+            <TodoCreate onCreateTodo={eventCreateTodo} />
+            <TodoList dataTodos={getTodos} />
+        </div>
+    )
+}
+
+export default Todo;
